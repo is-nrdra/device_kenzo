28dd7fc82d79a7875b87d710c19f6763c09272c4
Magicxavi <rolfpeterkesche@gmail.com>
2020-11-03 07:29:53 +0200

    kenzo: add ims_ext_common permission

--- device.mk
+++ device.mk
@@ -308,6 +308,10 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_PACKAGES += \
+    libbase_shim \
+    ims-ext-common \
+    ims_ext_common.xml \
+    ims-ext-common_system \
     android.hardware.radio@1.0 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
