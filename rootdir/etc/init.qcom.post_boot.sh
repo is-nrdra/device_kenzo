ee5bed0f8ec9164b5ce4fa0ba3a719656d08549d
ꦏꦸꦭꦺꦴꦤꦸꦮꦸꦤ꧀ <65087921+tragedy31@users.noreply.github.com>
2021-02-19 11:37:34 +0700

    kenzo : enabled overcommit ratio
    Useful for balancing swap work for kernel to move processes out of physical memory and to the swap disk. Since disk is much slower than RAM it can lead to slower response times for system and applications.

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -76,6 +76,7 @@ echo 0 >  /sys/module/lowmemorykiller/parameters/lmk_fast_run
 echo 1024 > /sys/module/process_reclaim/parameters/per_swap_size
 echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
 echo 55 > /proc/sys/vm/swappiness
+echo 75 > /proc/sys/vm/overcommit_ratio
 echo 0 > /proc/sys/vm/page-cluster
 
 minfree_series=`cat /sys/module/lowmemorykiller/parameters/minfree`
